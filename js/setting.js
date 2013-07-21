c1cd9b0edeaf6aff272cb202c9d862cc8cab70a4
Yasuharu OISHI <yasuharu.oishi@gmail.com>
2013-07-22 00:33:30 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -37,14 +37,16 @@ OpenSpending.Styles.Cofog = {
   '5-1': { icon: 'icons/tree.svg', color: '#C75746', bcolor: '#2A3A03'  },
   '5-2': { icon: 'icons/wind.svg', color: '#C75746', bcolor: '#2A3A03'  },
   '6': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#EC2406' },
-  '6-1': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#EC2406' },
+  '6010': { icon: 'icons/c_nougyou.svg', color: '#C75746', bcolor: '#EC2406' },
+  '6020': { icon: 'icons/forest.svg', color: '#C75746', bcolor: '#EC2406' },
+  '6030': { icon: 'icons/harbor.svg', color: '#C75746', bcolor: '#EC2406' },
   '7': { icon: 'icons/traffic-watersup.svg', color: '#C75746', bcolor: '#938626' },
   '7-1': { icon: 'icons/traffic-watersup.svg', color: '#C75746', bcolor: '#938626' },
   '8': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
   '8-1': { icon: 'icons/publicaffairs.svg', color: '#C75746', bcolor: '#C75746' },
   '8-2': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#C75746' },
   '9': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#D33673' },
-  '9-1': { icon: 'icons/garbage.svg', color: '#C75746', bcolor: '#D33673' },
+  '9010': { icon: 'icons/c_shyoubou.svg', color: '#C75746', bcolor: '#D33673' },
   '10': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' },
   '10-1': { icon: 'icons/order-safety.svg', color: '#C75746', bcolor: '#790586' }
 };
